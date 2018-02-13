0a210ef99ade5e99e01077f7b7ee77780fd718b2
Sven Guenther <sven.guenther@gmail.com>
2018-02-13 14:26:50 +0100

    Add a unmute script and install the on login hook1

--- installMuteMac.sh
+++ installMuteMac.sh
@@ -13,6 +13,12 @@ if [[ $? -eq 0 ]]; then
   exit -1
 fi
 
+sudo defaults read com.apple.loginwindow LoginHook 2> /dev/null
+if [[ $? -eq 0 ]]; then
+  echo "There is allready a login hook installed please check manualy"
+  exit -1
+fi
+
 #
 # create bin and script
 #
@@ -29,6 +35,18 @@ osascript -e 'set volume with output muted'
 EOF
 chmod u+x ~/bin/mute-mac.sh
 
+if [ -f ~/bin/unmute-mac.sh ]; then
+  echo "There is allready a script with the name unmute-mac.sh"
+  exit -2
+fi
+
+cat <<EOF > ~/bin/unmute-mac.sh
+#!/bin/bash
+osascript -e 'set volume without output muted'
+EOF
+chmod u+x ~/bin/unmute-mac.sh
+
+sudo defaults write com.apple.loginwindow LoginHook ~/bin/unmute-mac.sh
 sudo defaults write com.apple.loginwindow LogoutHook ~/bin/mute-mac.sh
 if [[ $? -eq 0 ]]; then
   echo "Installation successfull"
